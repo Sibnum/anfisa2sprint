1e1fd1df212118d3c5c39938919f1fe29e7026b5
Igor Zinchenko <sibnum@yandex.ru>
2025-12-19 22:09:15 +0300

    fix price

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
@@ -7,10 +6,12 @@ from ice_cream.models import IceCream
 def index(request):
     template_name = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'category__title'
+        'id', 'title', 'category__price', 'description'
     ).filter(
-        is_published=True, is_on_main=True
-    ).order_by('title')[1:4]
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
+    )
     context = {
         'ice_cream_list': ice_cream_list
     }
